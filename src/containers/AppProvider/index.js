82971139ff691620eed2e46846b2a5f688de5bfb
soupette <cyril.lpz@gmail.com>
2020-05-05 09:54:51 +0200

    Clean files
    Signed-off-by: soupette <cyril.lpz@gmail.com>

--- src/containers/AppProvider/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import AppContext from '../../contexts/AppContext';
-
-function AppProvider({ children, ...rest }) {
-  return <AppContext.Provider value={rest}>{children}</AppContext.Provider>;
-}
-
-export default AppProvider;
--- src/hooks/useApp.js
+++
@@ -1,6 +0,0 @@
-import { useContext } from 'react';
-import AppContext from '../contexts/AppContext';
-
-const useApp = () => useContext(AppContext);
-
-export default useApp;
